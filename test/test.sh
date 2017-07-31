0d96cd07754544c0fde1bdee4c29775301cc1303
pocomane <pocomane@nomail.com>
2017-07-31 20:27:26 +0200

    A simple test was added

---
+++ test/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -fR ./tmp
+mkdir -p ./tmp
+cd ./tmp
+
+gcc -std=c99 -o binject.exe ../*.c
+./binject.exe my_script
+
+echo "hello world" > my_text.txt
+./binject.exe my_text.txt
+rm my_text.txt
+
+./my_text.txt.exe > my_test.rpt
+
+RES=$(cat my_test.rpt | tail -n 2 | head -n 1)
+
+if [ "$RES" = "A 12 byte script was found (dump:)[hello world" ] ; then
+  echo "All tests passed"
+else
+  echo "TEST FAIL"
+fi
+
